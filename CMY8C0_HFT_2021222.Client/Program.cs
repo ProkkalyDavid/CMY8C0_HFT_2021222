702e6c58187f51ad87d87d702848761540dd99d0
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-03-18 19:24:40 +0100

    Add project files.

---
+++ CMY8C0_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMY8C0_HFT_2021222.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
