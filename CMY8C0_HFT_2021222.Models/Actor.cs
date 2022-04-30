17e48824a126cef9b4be306e51603618b0d05388
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-04-30 17:40:23 +0200

    Put context int he model classes, MovieDbContext created

--- CMY8C0_HFT_2021222.Models/Actor.cs
+++ CMY8C0_HFT_2021222.Models/Actor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace CMY8C0_HFT_2021222.Models
 {
     public class Actor
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ActorId { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string ActorName { get; set; }
+        public virtual ICollection<Movie> Movies { get; set; }
+        public Actor()
+        {
+
+        }
     }
 }
