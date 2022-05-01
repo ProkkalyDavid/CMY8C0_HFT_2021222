57ed09be93986d3fc6a44bfa5bd8d7e06c79d185
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-01 18:23:55 +0200

    Repository interfaces created, context put in them

---
+++ CMY8C0_HFT_2021222.Repository/ICarRepository.cs
@@ -0,0 +1,18 @@
+﻿using CMY8C0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMY8C0_HFT_2021222.Repository
+{
+    internal interface ICarRepository
+    {
+        void Create(Car car);
+        Car Read(int id);
+        IQueryable<Car> ReadAll();
+        void Update(Car car);
+        void Delete(int id);
+    }
+}
