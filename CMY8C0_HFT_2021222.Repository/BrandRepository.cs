ce0854206c09015bc6798537b3d3450bac499ab4
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-02 16:08:34 +0200

    BrandRepository added

---
+++ CMY8C0_HFT_2021222.Repository/BrandRepository.cs
@@ -0,0 +1,31 @@
+﻿using CMY8C0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMY8C0_HFT_2021222.Repository
+{
+    public class BrandRepository : Repository<Brand>, IRepository<Brand>
+    {
+        public BrandRepository(CarsDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Brand Read(int id)
+        {
+            return ctx.Brands.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Brand item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
