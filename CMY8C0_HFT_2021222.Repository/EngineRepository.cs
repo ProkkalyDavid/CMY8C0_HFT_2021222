4a566afcbabf1242933f180b2c96ba4e9856c4c7
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-02 16:12:06 +0200

    EngineRepository added

---
+++ CMY8C0_HFT_2021222.Repository/EngineRepository.cs
@@ -0,0 +1,31 @@
+﻿using CMY8C0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMY8C0_HFT_2021222.Repository
+{
+    public class EngineRepository : Repository<Engine>, IRepository<Engine>
+    {
+        public EngineRepository(CarsDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Engine Read(int id)
+        {
+            return ctx.Engines.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Engine item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
