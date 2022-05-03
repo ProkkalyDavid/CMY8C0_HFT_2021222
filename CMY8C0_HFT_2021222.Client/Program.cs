2179736c47edd5acead459dd4c9db7bebb102460
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-03 18:58:12 +0200

    RestService added

--- CMY8C0_HFT_2021222.Client/Program.cs
+++ CMY8C0_HFT_2021222.Client/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
 using CMY8C0_HFT_2021222.Models;
-using CMY8C0_HFT_2021222.Repository;
-using CMY8C0_HFT_2021222.Logic;
 using ConsoleTools;
 
 namespace CMY8C0_HFT_2021222.Client
