47503aa5ccd7e8d3d1791ec387d253eccc4cc8c8
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-03 18:10:42 +0200

    CarController done

---
+++ CMY8C0_HFT_2021222.Endpoint/Controllers/CarController.cs
@@ -0,0 +1,49 @@
+﻿using CMY8C0_HFT_2021222.Logic;
+using CMY8C0_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CMY8C0_HFT_2021222.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        ICarLogic logic;
+
+        public CarController(ICarLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Car> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Car Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Car value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Car value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
--- CMY8C0_HFT_2021222.Endpoint/Startup.cs
+++ CMY8C0_HFT_2021222.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using CMY8C0_HFT_2021222.Models;
+using CMY8C0_HFT_2021222.Logic;
+using CMY8C0_HFT_2021222.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,17 @@ namespace CMY8C0_HFT_2021222.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<CarsDbContext>();
+
+            services.AddTransient<IRepository<Car>, CarRepository>();
+            services.AddTransient<IRepository<Brand>, BrandRepository>();
+            services.AddTransient<IRepository<Engine>, EngineRepository>();
+
+            services.AddTransient<ICarLogic, CarLogic>();
+            services.AddTransient<IBrandLogic, BrandLogic>();
+            services.AddTransient<IEngineLogic, EngineLogic>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
