7ee2d4e51b5725664a4e34c4bfedeb0d6f7656ac
Prokkály Dávid <prokkaly@stud.uni-obuda.hu>
2022-05-03 16:46:43 +0200

    Folders for Logic layer added.

--- CMY8C0_HFT_2021222.Client/Program.cs
+++ CMY8C0_HFT_2021222.Client/Program.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using CMY8C0_HFT_2021222.Models;
 using CMY8C0_HFT_2021222.Repository;
 using CMY8C0_HFT_2021222.Logic;
-
+using ConsoleTools;
 
 namespace CMY8C0_HFT_2021222.Client
 {
@@ -11,28 +11,6 @@ namespace CMY8C0_HFT_2021222.Client
     {
         static void Main(string[] args)
         {
-            //test
-            var ctx = new CarsDbContext();
-            var repo = new CarRepository(ctx);
-            var logic = new CarLogic(repo);
-
-            logic.Create(new Car()
-            {
-                BrandId = 1,
-                EngineId = 1,
-                Km = 4000,
-                Id = 6,
-                Name = "M5 Competiton",
-                Year = 2021
-            });
-            var all = logic.ReadAll().ToArray();
-            var mileage = logic.HighestMileage().ToArray();
-            var cars = logic.CarsByBrands().ToArray();
-            var V8 = logic.CasWithV8().ToArray();
-            var germans = logic.GermanPremium().ToArray();
-            var oldest = logic.OldestCar().ToArray();
-
-            ;
         }
     }
 }
