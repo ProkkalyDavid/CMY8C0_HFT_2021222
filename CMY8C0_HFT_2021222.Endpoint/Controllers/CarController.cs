410c3d465e774409fd64f55ac5957a62eb2a99d6
prokkaly <prokkaly@stud.uni-obuda.hu>
2022-11-15 17:13:29 +0100

    Controllers done

--- CMY8C0_HFT_2021222.Endpoint/Controllers/CarController.cs
+++ CMY8C0_HFT_2021222.Endpoint/Controllers/CarController.cs
@@ -1,6 +1,9 @@
-﻿using CMY8C0_HFT_2021222.Logic;
+﻿using CMY8C0_HFT_2021222.Endpoint.Services;
+using CMY8C0_HFT_2021222.Logic;
 using CMY8C0_HFT_2021222.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,6 +15,7 @@ namespace CMY8C0_HFT_2021222.Endpoint.Controllers
     public class CarController : ControllerBase
     {
         ICarLogic logic;
+        IHubContext<SignalRHub> hub;
 
         public CarController(ICarLogic logic)
         {
@@ -37,6 +41,7 @@ namespace CMY8C0_HFT_2021222.Endpoint.Controllers
         public void Create([FromBody] Car value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("CarCreated", value);
         }
 
         // PUT api/<CarController>/5
@@ -44,13 +49,16 @@ namespace CMY8C0_HFT_2021222.Endpoint.Controllers
         public void Update([FromBody] Car value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("CarUpdated", value);
         }
 
         // DELETE api/<CarController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var carToDelete = logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("CarDeleted", carToDelete);
         }
     }
 }
