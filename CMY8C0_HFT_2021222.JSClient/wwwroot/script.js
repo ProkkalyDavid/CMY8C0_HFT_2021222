65ee4c1238c85cc5ac3c4dce8e7d1c6cd6813ccd
prokkaly <prokkaly@stud.uni-obuda.hu>
2022-11-17 02:40:44 +0100

    JSClient POST function (kinda) added

--- CMY8C0_HFT_2021222.JSClient/wwwroot/script.js
+++ CMY8C0_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,15 +1,36 @@
 ﻿let cars = [];
-fetch('http://localhost:43002/car')
-    .then(x => x.json())
-    .then(y => {
-        cars = y;
-        console.log(cars);
-        display();
-    });
+
+getdata();
+
+async function getdata() {
+    await fetch('http://localhost:43002/car')
+        .then(x => x.json())
+        .then(y => {
+            cars = y;
+            console.log(cars);
+            display();
+        });
+}
 
 function display() {
     cars.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>" + t.name + "</td></tr>";
     });
+}
+
+function create() {
+    let name = document.getElementById('carName').value;
+    fetch('http://localhost:43002/car', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { name: name})})
+        .then(response => response)
+        .then(data =>
+        {
+            console.log('Succes:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); })
 }
